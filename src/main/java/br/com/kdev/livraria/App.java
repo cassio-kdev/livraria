eb29e886e72a14a78ffc751dac1c6b34558ad40f
Cássio Alves Silva <cassio.alves09@gmail.com>
2021-03-08 07:38:23 -0300

    duplicano sysout

--- src/main/java/br/com/kdev/livraria/App.java
+++ src/main/java/br/com/kdev/livraria/App.java
@@ -3,5 +3,6 @@ package br.com.kdev.livraria;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
+        System.out.println("Hello World!!!");
     }
 }
